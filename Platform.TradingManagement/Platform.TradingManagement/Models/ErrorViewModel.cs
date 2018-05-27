83a23df45454384a195c6a8f1ce0d42e8d3c29f4
piotr.palic <skipper_tm@outlook.com>
2018-05-28 02:53:23 +0300

    Additional libs

---
+++ Platform.TradingManagement/Platform.TradingManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Platform.TradingManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- Platform.TradingManagement/Platform.TradingManagement/Startup.cs
+++ Platform.TradingManagement/Platform.TradingManagement/Startup.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace Platform.TradingManagement
 {
@@ -29,10 +23,22 @@ namespace Platform.TradingManagement
         {
             if (env.IsDevelopment())
             {
+                app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
 
-            app.UseMvc();
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
