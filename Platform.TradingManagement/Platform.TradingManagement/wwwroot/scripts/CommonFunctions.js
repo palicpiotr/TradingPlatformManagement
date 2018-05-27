90a68918c46c8f531c1e24547a58e248c3253cd7
piotr.palic <skipper_tm@outlook.com>
2018-05-28 02:34:00 +0300

    Add/Configure fonts, scripts, vendors Gentelella Alela

---
+++ Platform.TradingManagement/Platform.TradingManagement/wwwroot/scripts/CommonFunctions.js
@@ -0,0 +1,118 @@
+﻿function setCookie(cookieName, cookieValue, lifespanInDays, validDomain) {
+    var domainString = validDomain ? ("; domain=" + validDomain) : '';
+    document.cookie = cookieName
+        + "=" + encodeURIComponent(cookieValue)
+        + "; max-age=" + 60 * 60 * 24 * lifespanInDays
+        + "; path=/" + domainString;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) == 0) return decodeURIComponent(c.substring(name.length, c.length));
+    }
+    return "";
+}
+
+
+function sendRequest(url, method, data, dataType, callbacks) {
+
+    $(".loading-spinner").show();
+
+    $.ajax({
+
+        url: url,
+        type: method,
+        data: JSON.stringify(data),
+        dataType: dataType,
+        contentType: "application/json; charset=utf-8",
+
+        success: function (data) {
+            $(".loading-spinner").hide();
+
+            callbacks.onSCallback(data);
+        },
+
+        error: function (data) {
+            $(".loading-spinner").hide();
+
+            callbacks.onECallback(data);
+        },
+
+        statusCode: {
+
+            401: function () { authenticationRequire(); $("#details").empty(); $(".loading-spinner").hide(); }
+        }
+    });
+}
+
+function showPopupMessage(head, message, type) {
+
+    new PNotify({
+
+        'title': head,
+        'text': message,
+        'type': type,
+        'styling': 'bootstrap3'
+    });
+}
+
+function getErrorMessage(error) {
+    if (!error.errors && !error.Errors)
+        return true;
+
+    var key = error.errors == undefined ? "Errors" : "errors";
+
+    Object.keys(error[key]).forEach(
+
+        function (message, index, arr) {
+
+            showResultWindow(error[key][message], 0);
+        });
+}
+
+//parse string value from server to bool 
+function parseBool(val) {
+    if ((typeof val === 'string' && (val.toLowerCase() === 'true' || val.toLowerCase() === 'yes')) || val === 1)
+        return true;
+    else if ((typeof val === 'string' && (val.toLowerCase() === 'false' || val.toLowerCase() === 'no')) || val === 0)
+        return false;
+    return null;
+}
+
+
+/**
+ * Grid error handling
+ */
+function gridErrorHandler(e) {
+    riseError(e);
+    cancelGridChanges(e);
+}
+
+function riseError(e) {
+    var message = "";
+    if (e.errors) {
+        $.each(e.errors, function (key, value) {
+            message += this + "\n";
+        });
+    }
+    else {
+        message += 'UnknownError';
+    }
+    errorMessage(message);
+}
+
+function cancelGridChanges(e) {
+    var grid = e.sender;
+    grid.cancelChanges();
+}
+
+function errorMessage(message) {
+    showPopupMessage('Error', message, 'error');
+}
+
+function successMessage(message) {
+    showPopupMessage('Error', message, 'success');
+}
---
+++ Platform.TradingManagement/Platform.TradingManagement/wwwroot/scripts/SystemParameterTypes/SPT.js
@@ -0,0 +1,16 @@
+﻿function onGridDataSourceRequestEnd(e) {
+    if (e.type == "update" || e.type == "destroy" || e.type == "Cancel" || e.type == "create") {
+        this.read();
+    }
+
+}
+
+function onSystemParametersGridEdit(e) {
+    basePerson.PersonId = e.model.Persons.PersonId;
+    basePerson.PersonName = e.model.Persons.PersonName;
+    basePerson.PersonType = e.model.Persons.PersonType;
+
+    if (!e.model.isNew()) {
+        e.container.find('[data-role=dropdownlist]').data().kendoDropDownList.enable(false);
+    }
+}
