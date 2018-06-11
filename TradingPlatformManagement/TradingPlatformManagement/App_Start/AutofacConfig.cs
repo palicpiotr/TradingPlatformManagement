6144189ec4ba0177c365c8fc5318f446a74fa47c
Piotr Palic <skipper_tm@outlook.com>
2018-06-11 14:38:27 +0300

    configure DI Autofac and NLog

---
+++ TradingPlatformManagement/TradingPlatformManagement/App_Start/AutofacConfig.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Web.Mvc;
+using Autofac.Integration.Mvc;
+using TradingPlatformManagement.Controllers;
+
+namespace TradingPlatformManagement.App_Start
+{
+    public class AutofacConfig
+    {
+        public static void ConfigureDI() =>
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(BuildContainer()));
+
+        private static IContainer BuildContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<HomeController>().InstancePerDependency();
+
+            return builder.Build();
+        }
+    }
+}
