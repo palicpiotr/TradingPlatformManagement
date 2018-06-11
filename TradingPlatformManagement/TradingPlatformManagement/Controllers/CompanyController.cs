6b3bd78d7231f4b655a56b75de34810d073719d3
Piotr Palic <skipper_tm@outlook.com>
2018-06-11 23:09:50 +0300

    bidding filters

--- TradingPlatformManagement/TradingPlatformManagement/Controllers/CompanyController.cs
+++ TradingPlatformManagement/TradingPlatformManagement/Controllers/CompanyController.cs
@@ -1,17 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using TPM.DataAccessFramework.Providers.Companies;
 
 namespace TradingPlatformManagement.Controllers
 {
     public class CompanyController : Controller
     {
-        // GET: Company
+
+        private readonly ICompanyProvider _companyProvider;
+
+        public CompanyController(ICompanyProvider companyProvider)
+        {
+            _companyProvider = companyProvider;
+        }
+
         public ActionResult Index()
         {
             return View();
         }
+
+        public async Task<ActionResult> GetCompanies()
+        {
+            var companies = await _companyProvider.GetCompanies();
+            return Json(companies, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
--- TradingPlatformManagement/TradingPlatformManagement/Controllers/CountryController.cs
+++ TradingPlatformManagement/TradingPlatformManagement/Controllers/CountryController.cs
@@ -1,17 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using TPM.DataAccessFramework.Providers.Countries;
 
 namespace TradingPlatformManagement.Controllers
 {
     public class CountryController : Controller
     {
-        // GET: Country
+
+        private readonly ICountryProvider _countryProvider;
+
+        public CountryController(ICountryProvider countryProvider)
+        {
+            _countryProvider = countryProvider;
+        }
+
         public ActionResult Index()
         {
             return View();
         }
+
+        public async Task<ActionResult> GetCountries()
+        {
+            var countries = await _countryProvider.GetCountries();
+            return Json(countries, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
